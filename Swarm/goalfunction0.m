4bf9539cd37dee6c2c14754a1942a5e140822501
Arijit Kr Haldar <84299035+ArijitKHaldar@users.noreply.github.com>
2021-11-03 11:26:38 +0530

    Added sources and code

---
+++ Swarm/goalfunction0.m
@@ -0,0 +1,11 @@
+% Goal function: 
+% Author: Y. Liu, Version: 03/11/03
+function Jg=goalfunction0(x,xgoal,w2)
+% Let m be the dimension of the space and n the number of "agents".
+% Here xgoal is a mx1 vector indicates the position of the goal. 
+% x is a mxn matrix, indicating the positions of n points in a m-dim space.
+% And Jg returns a 1xn vector containing the cost function of those n points.
+[m,n]=size(x); 
+
+Err0= x-xgoal*ones(1,n);
+Jg=w2*sum(Err0.*Err0);
