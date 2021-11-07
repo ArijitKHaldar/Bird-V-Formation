0351377bc3aa0e614d1b48f0ae4a175a84eb64dd
Arijit Kr Haldar <84299035+ArijitKHaldar@users.noreply.github.com>
2021-11-07 13:02:38 +0530

    Initialized function with hard-coded coordinates

---
+++ Swarm/trianglecoordinates.m
@@ -0,0 +1,10 @@
+% Here I will try to return N coordinates of triangle as array for vertex
+% angle as parameter
+function ft=trianglecoordinates(num,angl)
+     arr=zeros(num,2);
+     arr=[7,6.5;
+            6,4;
+            8,4;
+            9,2;
+            5,2];
+     ft=arr;
