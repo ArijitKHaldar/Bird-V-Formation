bd43212cbd885f1aef675533538f8d36f7dfbd6f
Arijit Kr Haldar <84299035+ArijitKHaldar@users.noreply.github.com>
2021-11-30 17:50:57 +0530

    Corrected Function Name in code

--- Swarm/triangleAgents.m
+++ Swarm/triangleAgents.m
@@ -1,6 +1,6 @@
 % Here I will try to return N coordinates of triangle as array for vertex
 % angle as parameter
-function ft=trianglecoordinates(num,triangle_vertex)
+function ft=triangleAgents(num,triangle_vertex)
      arr=zeros(num,2);
      arr=[triangle_vertex(1,1),triangle_vertex(1,2);
             1.3,4.7;
