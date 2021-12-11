7bbf226ad39e018171471481dc1f23d91861e231
Arijit Kumar Haldar <84299035+ArijitKHaldar@users.noreply.github.com>
2021-12-11 10:30:17 +0530

    Simplified conditional equations

--- Swarm/trianglePeak.m
+++ Swarm/trianglePeak.m
@@ -1,6 +1,13 @@
 function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
-    if(n == 1)
-       [xtemp,ytemp] = linecirc(((0-P(1,2))/(0-P(1,1))),0,P(1,1),P(1,2),P(1,3)-3);
+    if(n == 1) % For first iteration, align using global origin
+       x1 = P(1,1);
+       x2 = 0;
+       y1 = P(1,2);
+       y2 = 0;
+       m = (y2-y1)/(x2-x1);
+       r = P(1,3)-3;
+       yintercept = 0;
+       [xtemp,~] = linecirc(m,yintercept,x1,y1,r);
        if xtemp(1,1) > xtemp(1,2)
            minimum =1000;
            for i=1:1:length(Circle_Co)
@@ -9,6 +16,12 @@ function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
            end
+       figure    
+       plot(Coor(1,1),Coor(1,2),'r*');
+       hold on
+       plot(0,0,'g*');
+       plot(Circle_Co(:,1),Circle_Co(:,2));
+       hold off           
        else
            minimum =1000;
            for i=1:1:length(Circle_Co)
@@ -17,9 +30,24 @@ function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
            end
+       figure    
+       plot(Coor(1,1),Coor(1,2),'r*');
+       hold on
+       plot(0,0,'g*');
+       plot(Circle_Co(:,1),Circle_Co(:,2));
+       hold off
        end
-    else
-        [xtemp,ytemp] = linecirc(((cirCenter(n-1,2)-P(1,2))/(cirCenter(n-1,1)-P(1,1))),((P(1,2)*cirCenter(n-1,1)-cirCenter(n-1,2)*P(1,1))/(cirCenter(n-1,1)-P(1,1))),P(1,1),P(1,2),P(1,3)-3);
+       
+       
+    else % For later, align using previous circle centre
+        x1 = P(1,1);
+        x2 = cirCenter(n-1,1);
+        y1 = P(1,2);
+        y2 = cirCenter(n-1,2);
+        m = (y2-y1)/(x2-x1);
+        r = P(1,3)-3;
+        yintercept = (y1*x2-y2*x1)/(x2-x1);
+        [xtemp,~] = linecirc(m,yintercept,x1,y1,r);
         if xtemp(1,1) > xtemp(1,2)
            minimum =1000;
            for i=1:1:length(Circle_Co)
@@ -28,6 +56,12 @@ function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
            end
+       figure    
+       plot(Coor(1,1),Coor(1,2),'r*');
+       hold on
+       plot(cirCenter(n-1,1),cirCenter(n-1,2),'g*');
+       plot(Circle_Co(:,1),Circle_Co(:,2));
+       hold off 
        else
            minimum =1000;
            for i=1:1:length(Circle_Co)
@@ -37,6 +71,12 @@ function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
                end
            end
        end
+       figure    
+       plot(Coor(1,1),Coor(1,2),'r*');
+       hold on
+       plot(cirCenter(n-1,1),cirCenter(n-1,2),'g*');
+       plot(Circle_Co(:,1),Circle_Co(:,2));
+       hold off 
     end
     coordinate = Coor(1,:);
 end
