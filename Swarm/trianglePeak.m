0cf23d980ac4e0737a7ca0ad409ebd869ab9bf74
Arijit Kumar Haldar <84299035+ArijitKHaldar@users.noreply.github.com>
2021-12-11 10:38:16 +0530

    Corrected error where I used xtemp instead of ytemp for y coordinate

--- Swarm/trianglePeak.m
+++ Swarm/trianglePeak.m
@@ -7,35 +7,23 @@ function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
        m = (y2-y1)/(x2-x1);
        r = P(1,3)-3;
        yintercept = 0;
-       [xtemp,~] = linecirc(m,yintercept,x1,y1,r);
+       [xtemp,ytemp] = linecirc(m,yintercept,x1,y1,r);
        if xtemp(1,1) > xtemp(1,2)
            minimum =1000;
            for i=1:1:length(Circle_Co)
-               if(sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-xtemp(1,1),2)) < minimum)
-                   minimum = sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-xtemp(1,1),2));
+               if(sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-ytemp(1,1),2)) < minimum)
+                   minimum = sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-ytemp(1,1),2));
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
-           end
-       figure    
-       plot(Coor(1,1),Coor(1,2),'r*');
-       hold on
-       plot(0,0,'g*');
-       plot(Circle_Co(:,1),Circle_Co(:,2));
-       hold off           
+           end         
        else
            minimum =1000;
            for i=1:1:length(Circle_Co)
-               if(sqrt(power(Circle_Co(i,1)-xtemp(1,2),2)+power(Circle_Co(i,2)-xtemp(1,2),2)) < minimum)
-                   minimum = sqrt(power(Circle_Co(i,2)-xtemp(1,2),2)+power(Circle_Co(i,2)-xtemp(1,2),2));
+               if(sqrt(power(Circle_Co(i,1)-xtemp(1,2),2)+power(Circle_Co(i,2)-ytemp(1,2),2)) < minimum)
+                   minimum = sqrt(power(Circle_Co(i,2)-xtemp(1,2),2)+power(Circle_Co(i,2)-ytemp(1,2),2));
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
            end
-       figure    
-       plot(Coor(1,1),Coor(1,2),'r*');
-       hold on
-       plot(0,0,'g*');
-       plot(Circle_Co(:,1),Circle_Co(:,2));
-       hold off
        end
        
        
@@ -47,36 +35,24 @@ function coordinate = trianglePeak(n,P,cirCenter,Circle_Co)
         m = (y2-y1)/(x2-x1);
         r = P(1,3)-3;
         yintercept = (y1*x2-y2*x1)/(x2-x1);
-        [xtemp,~] = linecirc(m,yintercept,x1,y1,r);
+        [xtemp,ytemp] = linecirc(m,yintercept,x1,y1,r);
         if xtemp(1,1) > xtemp(1,2)
            minimum =1000;
            for i=1:1:length(Circle_Co)
-               if(sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-xtemp(1,1),2)) < minimum)
-                   minimum = sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-xtemp(1,1),2));
+               if(sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-ytemp(1,1),2)) < minimum)
+                   minimum = sqrt(power(Circle_Co(i,1)-xtemp(1,1),2)+power(Circle_Co(i,2)-ytemp(1,1),2));
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
            end
-       figure    
-       plot(Coor(1,1),Coor(1,2),'r*');
-       hold on
-       plot(cirCenter(n-1,1),cirCenter(n-1,2),'g*');
-       plot(Circle_Co(:,1),Circle_Co(:,2));
-       hold off 
        else
            minimum =1000;
            for i=1:1:length(Circle_Co)
-               if(sqrt(power(Circle_Co(i,1)-xtemp(1,2),2)+power(Circle_Co(i,2)-xtemp(1,2),2)) < minimum)
-                   minimum = sqrt(power(Circle_Co(i,2)-xtemp(1,2),2)+power(Circle_Co(i,2)-xtemp(1,2),2));
+               if(sqrt(power(Circle_Co(i,1)-xtemp(1,2),2)+power(Circle_Co(i,2)-ytemp(1,2),2)) < minimum)
+                   minimum = sqrt(power(Circle_Co(i,2)-xtemp(1,2),2)+power(Circle_Co(i,2)-ytemp(1,2),2));
                    Coor(1,:) = [Circle_Co(i,1),Circle_Co(i,2)];
                end
            end
-       end
-       figure    
-       plot(Coor(1,1),Coor(1,2),'r*');
-       hold on
-       plot(cirCenter(n-1,1),cirCenter(n-1,2),'g*');
-       plot(Circle_Co(:,1),Circle_Co(:,2));
-       hold off 
+        end
     end
     coordinate = Coor(1,:);
 end
